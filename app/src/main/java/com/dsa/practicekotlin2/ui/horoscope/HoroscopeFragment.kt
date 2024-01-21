0462fa0e44d98f5373a39a04eeba53e7d98ee0a4
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-20 19:45:52 -0500

    adding navigation to fragments horoscope, luck and palmistry

---
+++ app/src/main/java/com/dsa/practicekotlin2/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,20 @@
+package com.dsa.practicekotlin2.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dsa.practicekotlin2.R
+
+class HoroscopeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_horoscope, container, false)
+    }
+
+}
