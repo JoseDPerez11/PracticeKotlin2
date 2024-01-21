9b807e790a2a1b3605d99cce3909d70444ca3f97
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-20 21:39:40 -0500

    adding retrofit dependencies

---
+++ app/src/main/java/com/dsa/practicekotlin2/data/network/HoroscopeApiService.kt
@@ -0,0 +1,4 @@
+package com.dsa.practicekotlin2.data.network
+
+class HoroscopeApiService {
+}
---
+++ app/src/main/java/com/dsa/practicekotlin2/data/network/response/PredictionResponse.kt
@@ -0,0 +1,3 @@
+package com.dsa.practicekotlin2.data.network.response
+
+data class PredictionResponse()
